28fa2285a7491ff8048271894e9a926434f79dab
Jonathon Harrell <kaos78414@yahoo.com>
2024-08-15 18:06:58 -0500

    Test the service class

--- app/presenters/weather_report_presenter.rb
+++ app/presenters/weather_report_presenter.rb
@@ -4,4 +4,8 @@ class WeatherReportPresenter
   def initialize(weather_report)
     @weather_report ||= weather_report
   end
+
+  def cached?
+    weather_report.has_key?("cached_at")
+  end
 end
--- test/services/get_weather_report_test.rb
+++ test/services/get_weather_report_test.rb
@@ -1,15 +1,35 @@
 require "test_helper"
 
-class GetweatherReportTest < ActiveSupport::TestCase
+class GetWeatherReportTest < ActiveSupport::TestCase
+  zip = 90210
+
   setup do
-    # clear rails cache so we can test caching before/after
+    Rails.cache.clear
   end
 
-  test "#process provides the weather report for a given zip, along with cached at time for display purposes" do
-    assert false
+  test "#process either uses the cached weather report or grabs a fresh one from the lib" do
+    mock = Minitest::Mock.new
+    fake_result = { "success": true }
+    mock.expect(:generate, fake_result)
+    mock.expect(:generate, fake_result) # two calls, apparently there is not a better way to do this, this makes me want rspec back
+
+    WeatherReportGenerator.stub(:new, mock) do
+      result = GetWeatherReport.process(zip)
+      assert_instance_of WeatherReportPresenter, result
+      assert_equal fake_result, result.weather_report
+      assert_not result.cached?
+      assert_not_nil Rails.cache.fetch(zip)
+    end
   end
 
-  test "self.process creates a new instance and calls process on it" do
-    assert false
+  test "self.process creates a new instance and calls #process on it" do
+    mock = Minitest::Mock.new
+    mock.expect :process, true
+
+    GetWeatherReport.stub(:new, mock) do
+      assert GetWeatherReport.process(zip)
+    end
+    
+    mock.verify
   end
 end
